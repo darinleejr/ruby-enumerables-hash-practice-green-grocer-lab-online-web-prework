5eefd750241ab038f0b5cc324852623d9fd6e4a5
Darin Webster <darinleenavy@gmail.com>
2019-07-18 16:57:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,14 @@
 def consolidate_cart(cart)
-  # code here
+  cart = [find_item('TEMPEH'), find_item('PEANUTBUTTER'), find_item('ALMONDS')]
+      result = consolidate_cart(cart)
+      result.each do |item, attributes|
+        expect(attributes.keys).to include(:count)
+        expect(attributes[:count]).to eq(1)
+      end
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  
 end
 
 def apply_clearance(cart)
